219603bbc95c3e1a61827a6fe3955c497b97af65
Dmitriy Morozov <dmitriy@mrzv.org>
2021-09-01 21:03:22 -0700

    Add is_dir to same(Change,Change) comparison

--- src/scan/change.rs
+++ src/scan/change.rs
@@ -38,11 +38,11 @@ pub fn same(x: &Change, y: &Change) -> bool {
         (Change::Removed(_), Change::Removed(_)) => true,
         (Change::Added(d1), Change::Added(d2)) => {
             assert_eq!(d1.path, d2.path);
-            d1.size == d2.size && d1.mtime == d2.mtime && d1.mode == d2.mode && d1.target == d2.target
+            d1.size == d2.size && d1.mtime == d2.mtime && d1.mode == d2.mode && d1.target == d2.target && d1.is_dir == d2.is_dir
         },
         (Change::Modified(_,d1), Change::Modified(_,d2)) => {
             assert_eq!(d1.path, d2.path);
-            d1.size == d2.size && d1.mtime == d2.mtime && d1.mode == d2.mode && d1.target == d2.target
+            d1.size == d2.size && d1.mtime == d2.mtime && d1.mode == d2.mode && d1.target == d2.target && d1.is_dir == d2.is_dir
         },
         _ => false,
     }
